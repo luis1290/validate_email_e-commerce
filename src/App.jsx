06220fda81b750b2692ecb89d0316898192456a1
Luis Adan Gomez Torres <lgomez1290@gmail.com>
2023-06-09 22:37:50 -0600

    cambiando ruta

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 import RegisterForm from './forms/RegisterForm'
 import ValidateEmail from './ValidateEmail'
 import Hola from './Componet'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/hola' element={<Hola />} />
         {/* <Route path='/email-validate' element={<ValidateEmail />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
